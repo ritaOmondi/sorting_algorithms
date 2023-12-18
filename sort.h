9a0cadd7fae813472bac40a60e983692c7cbedaa
ritaOmondi <cherylritaomondi@gmail.com>
2023-12-18 03:29:29 -0800

    updated prototype

--- sort.h
+++ sort.h
@@ -33,6 +33,7 @@ void lomuto_qsort(int *array, int low, int high, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void merge(int *array, int *l, int *r, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
